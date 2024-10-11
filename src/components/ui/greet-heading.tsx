9623c2dd68aa72010081e50b5f010152cefd28df
gav1306 <gavk30@gmail.com>
2024-10-11 19:34:23 +0530

    greetings bug fixed

--- src/components/ui/greet-heading.tsx
+++ src/components/ui/greet-heading.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import { getGreetings } from "@/lib/date";
+import { useEffect, useState } from "react";
 
 const GreetHeading = () => {
+  const [greetings, setGreetings] = useState("");
+
+  useEffect(() => {
+    setGreetings(getGreetings());
+  }, []);
   return (
     <h1 className="text-3xl text-center lg:text-4xl text-text-dark mt-16">
-      Good {getGreetings()}, John Dane
+      Good {greetings}, John Dane
     </h1>
   );
 };
